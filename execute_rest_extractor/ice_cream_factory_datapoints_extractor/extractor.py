a1d78ae5dcd2cd83e048f16ed99f3913c1e2ae1f
Christian Flasshoff <71002867+chrfla-cognite@users.noreply.github.com>
2022-12-12 09:57:15 +0100

    use find_dotenv

--- execute_rest_extractor/ice_cream_factory_datapoints_extractor/extractor.py
+++ execute_rest_extractor/ice_cream_factory_datapoints_extractor/extractor.py
@@ -158,7 +158,7 @@ def main(config_file_path: str = "extractor_config.yaml") -> None:
     """
 
     import dotenv
-    dotenv.load_dotenv = lambda: ""
+    dotenv.find_dotenv = lambda: ""
     with Extractor(
         name="datapoints_rest_extractor",
         description="An extractor that ingest datapoints from the Ice Cream Factory API to CDF clean",
